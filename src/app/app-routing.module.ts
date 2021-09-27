b4770394cef60751028fd42a574dc511ccb6d5d7
gitSushi <lossushi37@gmail.com>
2021-09-27 10:30:50 +0000

    int dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./movies/list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'detail',
+    loadChildren: () => import('./movies/detail/detail.module').then(m => m.DetailPageModule)
   },
 ];
 
